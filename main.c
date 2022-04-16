cf23bed4a22d9a53d41a603efbbc1446ec94aeb2
Ak0p <baroiu.andrei13@gmail.com>
2022-04-16 18:58:52 +0300

    v1

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #define inputSize 256
 #define numberOfWords 8
 
+void *getPos(void*, int, int);
+
 typedef struct {
 	char *dedicator;
 	int8_t bancnota1;
@@ -92,14 +94,245 @@ else
 
 int add_at(void **arr, int *len, data_structure *data, int index) {
 
+	if (index < 0)
+		return 0;
+	if(realloc(*arr, (size_t)((size_t)len + (size_t)data->header->len) ) )
+		return 0;
+	void **offset = getPos(*arr, index, *len);
+	int afterLen = (int)((*arr) + *len - *offset); // numarul de byti care trebuie mutat
+	memmove(*offset + data->header->len, *offset, afterLen);
+
+	u_char tip = (u_char)data->header->type;
+	memcpy(*offset, &tip, sizeof(u_char));
+	*offset += sizeof(u_char);
+	memcpy(*offset, &tip, sizeof(u_int));
+	*offset += sizeof(u_int);
+	switch (tip) {
+		case 1:
+		{
+			tip1 *pAux = (tip1*)data->data;
+			memcpy(*offset, pAux->dedicator, strlen((char*)pAux->dedicator)+1);
+			*offset += strlen((char*)pAux->dedicator)+1;
+			memcpy(*offset, &pAux->bancnota1, sizeof(int8_t));
+			*offset += sizeof(int8_t);
+			memcpy(*offset, &pAux->bancnota2, sizeof(int8_t));
+			*offset += sizeof(int8_t);
+			memcpy(*offset, pAux->barosan, strlen((char*)pAux->barosan)+1);
+			*offset += strlen((char*)pAux->barosan)+1;
+			break;
+		}
+		case 2:
+		{
+			tip2 *pAux = (tip2*)data->data;
+			memcpy(*offset, pAux->dedicator, strlen((char*)pAux->dedicator)+1);
+			*offset += strlen((char*)pAux->dedicator)+1;
+			memcpy(*offset, &pAux->bancnota1, sizeof(int16_t));
+			*offset += sizeof(int16_t);
+			memcpy(*offset, &pAux->bancnota2, sizeof(int32_t));
+			*offset += sizeof(int32_t);
+			memcpy(*offset, pAux->barosan, strlen((char*)pAux->barosan)+1);
+			*offset += strlen((char*)pAux->barosan)+1;
+			break;
+		}
+		case 3:
+		{
+			tip3 *pAux = (tip3*)data->data;
+			memcpy(*offset, pAux->dedicator, strlen((char*)pAux->dedicator)+1);
+			*offset += strlen((char*)pAux->dedicator)+1;
+			memcpy(*offset, &pAux->bancnota1, sizeof(int32_t));
+			*offset += sizeof(int32_t);
+			memcpy(*offset, &pAux->bancnota2, sizeof(int32_t));
+			*offset += sizeof(int32_t);
+			memcpy(*offset, pAux->barosan, strlen((char*)pAux->barosan)+1);
+			*offset += strlen((char*)pAux->barosan)+1;
+			break;
+		}
+		default:
+			return 0;
+	}
+
+*len += data->header->len;
+if(*len - (int)(*offset - *arr) != 0)
+	return 1;
+else
+	return 0;
+
 }
 
 void find(void *data_block, int len, int index) {
 
+	if (index < 0)
+		return;
+	void *offset = getPos(data_block, index, len);
+		u_char *type = offset;
+		u_int *structLen = offset + 1;
+		offset += sizeof(u_char) + sizeof(u_int);
+		printf("Tipul %d", *type);
+		switch (*type) {
+			case 1:
+			{
+				char *primulNume = (char*)offset;
+				offset += strlen(primulNume) + 1;
+				int8_t *banc1 = (int8_t*)offset;
+				offset += sizeof(int8_t);
+				int8_t *banc2 = (int8_t*)offset;
+				offset += sizeof(int8_t);
+				char *alDoileaNume = (char*)offset;
+				offset += strlen(alDoileaNume) + 1;
+				printf("%s pentru %s\n", primulNume, alDoileaNume);
+				printf("%"PRId8"\n", *banc1);
+				printf("%"PRId8"\n", *banc2);
+				break;
+			}
+			case 2:
+			{
+				char *primulNume = (char*)offset;
+				offset += strlen(primulNume) + 1;
+				int16_t *banc1 = (int16_t*)offset;
+				offset += sizeof(int16_t);
+				int32_t *banc2 = (int32_t*)offset;
+				offset += sizeof(int32_t);
+				char *alDoileaNume = (char*)offset;
+				offset += strlen(alDoileaNume) + 1;
+				printf("%s pentru %s\n", primulNume, alDoileaNume);
+				printf("%"PRId16"\n", *banc1);
+				printf("%"PRId32"\n", *banc2);
+				break;
+			}
+			case 3:
+			{
+				char *primulNume = (char*)offset;
+				offset += strlen(primulNume) + 1;
+				int32_t *banc1 = (int32_t*)offset;
+				offset += sizeof(int32_t);
+				int32_t *banc2 = (int32_t*)offset;
+				offset += sizeof(int32_t);
+				char *alDoileaNume = (char*)offset;
+				offset += strlen(alDoileaNume) + 1;
+				printf("%s pentru %s\n", primulNume, alDoileaNume);
+				printf("%"PRId32"\n", *banc1);
+				printf("%"PRId32"\n", *banc2);
+				break;
+			}
+			default:
+				printf("Tip invalid de date\n");
+				return;
+		}
+		if (offset - data_block != *structLen)
+			printf("Eroare\n");
 }
 
 int delete_at(void **arr, int *len, int index) {
+	void **offset = getPos(*arr, index, *len);
+	int elLen = 0;
+	if(!memcpy(&elLen, *offset + 1, sizeof(int)))
+		return 0;
+	if(!memmove(*offset, *offset + elLen, elLen))
+		return 0;
+	*len -= elLen;
+	if(!realloc(*arr, *len))
+		return 0;
+	return 1;
+}
 
+void print(void *arr, int len) {
+	void *offset = arr;
+	// u_int elLen = 0;
+	// if(!memcpy(&elLen, offset + 1, sizeof(u_int)))
+	// 	return 0;
+	while(offset < arr + len) {
+		u_char *type = (u_char*)offset;
+		printf("Tipul %d\n", *type);
+
+		switch (*type) {
+			case 1:
+			{
+				char *primulNume = (char*)offset;
+				offset += strlen(primulNume) + 1;
+				int8_t *banc1 = (int8_t*)offset;
+				offset += sizeof(int8_t);
+				int8_t *banc2 = (int8_t*)offset;
+				offset += sizeof(int8_t);
+				char *alDoileaNume = (char*)offset;
+				offset += strlen(alDoileaNume) + 1;
+				printf("%s pentru %s\n", primulNume, alDoileaNume);
+				printf("%"PRId8"\n", *banc1);
+				printf("%"PRId8"\n", *banc2);
+				break;
+			}
+			case 2:
+			{
+				char *primulNume = (char*)offset;
+				offset += strlen(primulNume) + 1;
+				int16_t *banc1 = (int16_t*)offset;
+				offset += sizeof(int16_t);
+				int32_t *banc2 = (int32_t*)offset;
+				offset += sizeof(int32_t);
+				char *alDoileaNume = (char*)offset;
+				offset += strlen(alDoileaNume) + 1;
+				printf("%s pentru %s\n", primulNume, alDoileaNume);
+				printf("%"PRId16"\n", *banc1);
+				printf("%"PRId32"\n", *banc2);
+				break;
+			}
+			case 3:
+			{
+				char *primulNume = (char*)offset;
+				offset += strlen(primulNume) + 1;
+				int32_t *banc1 = (int32_t*)offset;
+				offset += sizeof(int32_t);
+				int32_t *banc2 = (int32_t*)offset;
+				offset += sizeof(int32_t);
+				char *alDoileaNume = (char*)offset;
+				offset += strlen(alDoileaNume) + 1;
+				printf("%s pentru %s\n", primulNume, alDoileaNume);
+				printf("%"PRId32"\n", *banc1);
+				printf("%"PRId32"\n", *banc2);
+				break;
+			}
+			default:
+				printf("Tip invalid de date\n");
+				return;
+		}
+	}
+}
+
+void freeStruct(void **el, u_char tip) {
+	switch(tip) {
+		case 1:
+		{
+			tip1 **pFree = (tip1**)el;
+			free((*pFree)->dedicator);
+			free((*pFree)->barosan);
+			free(el);
+			break;
+		}
+
+		case 2:
+		{
+			tip2 **pFree = (tip2**)el;
+			free((*pFree)->dedicator);
+			free((*pFree)->barosan);
+			free(el);
+			break;
+		}
+
+		case 3:
+		{
+			tip3 **pFree = (tip3**)el;
+			free((*pFree)->dedicator);
+			free((*pFree)->barosan);
+			free(el);
+			break;
+		}
+	}
+}
+
+void freeData(data_structure **el) {
+	u_char tip = (*el)->header->type;
+	free((*el)->header);
+	freeStruct((*el)->data, tip);
+	free(el);
 }
 
 
@@ -178,6 +411,18 @@ data_structure *genDataStruct (void *info, int type) {
 	return celula;
 }
 
+void *getPos(void *arr, int index, int len) {
+	void *offset = arr;
+	for(int i = 0; i < index && offset - arr <= len; i++) {
+		int *elLen = (int*)(offset + 1); // probabil sa dea seg fault daca index e mai mare decat nr de elem
+		offset += *elLen;
+	}
+	if(offset - arr > len)
+		offset = arr + len;
+	return offset;
+}
+
+
 int main() {
 	// the vector of bytes u have to work with
 	// good luck :)
@@ -209,7 +454,7 @@ cuvant[contorWordVec] = strtok(NULL, " ");
 // add last  block
 
 if (strcmp(cuvant[0], "insert") == 0) {
-	int8_t tip = (int8_t)atoi(cuvant[2]);
+	int8_t tip = (int8_t)atoi(cuvant[1]);
 	data_structure *element = NULL; // vezi poate ramane dangling pointer daca nu il faci null dupa
 	switch (tip) {
 
@@ -219,6 +464,8 @@ if (strcmp(cuvant[0], "insert") == 0) {
 			int8_t bancnota2 = (int8_t)atoi(cuvant[4]);
 			tip1 *pAux = genStruct1(cuvant[2], bancnota1, bancnota2, cuvant[5]);
 			element = genDataStruct(pAux, tip);
+			freeStruct((void**)&pAux, tip);
+			// free struct1 si datastruct
 			break;
 		}
 
@@ -226,9 +473,10 @@ if (strcmp(cuvant[0], "insert") == 0) {
 		{
 			int16_t bancnota1 = (int16_t)atoi(cuvant[3]);
 			int32_t bancnota2 = (int32_t)atoi(cuvant[4]);
-			genStruct2(cuvant[2], bancnota1, bancnota2, cuvant[5]);
 			tip2 *pAux = genStruct2(cuvant[2], bancnota1, bancnota2, cuvant[5]);
 			element = genDataStruct(pAux, tip);
+			freeStruct((void**)&pAux, tip);
+			// free struct2 si datastruct
 			break;
 		}
 
@@ -236,17 +484,69 @@ if (strcmp(cuvant[0], "insert") == 0) {
 		{
 			int32_t bancnota1 = (int32_t)atoi(cuvant[3]);
 			int32_t bancnota2 = (int32_t)atoi(cuvant[4]);
-			genStruct3(cuvant[2], bancnota1, bancnota2, cuvant[5]);
 			tip3 *pAux = genStruct3(cuvant[2], bancnota1, bancnota2, cuvant[5]);
 			element = genDataStruct(pAux, tip);
+			freeStruct((void**)&pAux, tip);
+			// free struct3 si datastruct
 			break;
 		}
 	}
 
-	if(add_last(&arr,&len, element))
+	if(!add_last(&arr,&len, element))
 		return 1;
+	freeData(&element);
 }
 
+// insert at block
+
+if (strcmp(cuvant[0], "insert_at") == 0) {
+
+
+	int index = atoi(cuvant[1]);
+	int8_t tip = (int8_t)atoi(cuvant[2]);
+	data_structure *element = NULL; // vezi poate ramane dangling pointer daca nu il faci null dupa
+	switch (tip) {
+
+		case 1:
+		{
+			int8_t bancnota1 = (int8_t)atoi(cuvant[4]);
+			int8_t bancnota2 = (int8_t)atoi(cuvant[5]);
+			tip1 *pAux = genStruct1(cuvant[3], bancnota1, bancnota2, cuvant[6]);
+			element = genDataStruct(pAux, tip);
+			// free struct1 si datastruct
+			break;
+		}
+
+		case 2:
+		{
+			int16_t bancnota1 = (int16_t)atoi(cuvant[4]);
+			int32_t bancnota2 = (int32_t)atoi(cuvant[5]);
+			tip2 *pAux = genStruct2(cuvant[3], bancnota1, bancnota2, cuvant[6]);
+			element = genDataStruct(pAux, tip);
+			// free struct2 si datastruct
+			break;
+		}
+
+		case 3:
+		{
+			int32_t bancnota1 = (int32_t)atoi(cuvant[4]);
+			int32_t bancnota2 = (int32_t)atoi(cuvant[5]);
+			tip3 *pAux = genStruct3(cuvant[3], bancnota1, bancnota2, cuvant[6]);
+			element = genDataStruct(pAux, tip);
+			// free struct3 si datastruct
+			break;
+		}
+	}
+
+	if(!add_at(&arr,&len, element, index))
+		return 1;
+	freeData(&element);
+
+}
+
+if (strcmp(cuvant[0], "print") == 0) {
+
+}
 
 
 //  exit block
@@ -254,28 +554,19 @@ if (strcmp(cuvant[0], "exit") == 0) {
 
 	// free vector //
 
-
-	contorWordVec = 0;
-	while(cuvant[contorWordVec]) {
-		free(cuvant[contorWordVec]);
-		contorWordVec++;
-	}
+	free(arr);
 	free(cuvant);
 	free(comanda);
 
-	return 1;
+	return 0;
 }
 
 
-// start dealocare vector de cuvinte din input
-contorWordVec = 0;
-while(cuvant[contorWordVec]) {
-	cuvant[contorWordVec] = NULL;
-	contorWordVec++;
-}
+// start dealocare input
+
 free(cuvant);
 free(comanda);
-// end dealocare vector de cuvinte din input
+// end dealocare input
 }
 
 
